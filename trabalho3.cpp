d1c412da746afb8cef7b02717580602ff62f17ff
joseduardo42 <joseduardosilva.42@gmail.com>
2021-07-23 10:25:54 -0300

    v1

---
+++ trabalho3.cpp
@@ -0,0 +1,16 @@
+#include "Arduino.h"
+#include "LiquidCrystal.h"
+
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2)
+
+void setup(){
+    lcd.begin(16,2);
+    lcd.clear();
+}
+
+void loop(){
+    lcd.setCursor(3,0);
+    lcd.print("OLA");
+    lcd.setCursor(4,1);
+    lcd.print("DELICIA")
+}
